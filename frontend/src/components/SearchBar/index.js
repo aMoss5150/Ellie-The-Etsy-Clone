a07e845ac76dd05945dad292c3775a0286b70cef
Andrew Moss <andrewmossaudio@gmail.com>
2021-06-12 22:42:17 -0500

    working search function

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -4,24 +4,44 @@ import { useSelector } from 'react-redux'
 import './SearchBar.css'
 
 export default function SearchBar() {
-    const [searchVal, setSearchVal] = useState('')
     const [inputVal, setInputVal] = useState('')
-
+    const [matches, setMatches] = useState([])
+    const [prodsArr, setProdsArr] = useState([])
+    
+    
     const products = useSelector(state => state.products)
-
+    
+    
     const handleSearch=()=> {
-        
+        let possibles = []
+        Object.values(products).forEach((product)=> {
+            let lengthOfSrch = inputVal.length
+            let possMatch = product.product_name.split('').slice(0,lengthOfSrch).join('')
+            console.log(possMatch)
+
+            if (possMatch.toLowerCase() === inputVal.toLowerCase() && inputVal.length > 0) {
+                possibles.push(product.product_name)
+            }
+        })
+
+        setMatches(possibles)
+        console.log(possibles);
+    }
+    const searchSubmit = () => {
+
     }
 
     useEffect(() => {
-        setSearchVal(inputVal)
+        handleSearch()
     }, [inputVal])
 
+    // setProdsArr(Array.from(products))
     if (!products) return null
+
     else return (
         <div className='search__bar-parent'>
-            <form className='test-form' action="" onSubmit={handleSearch}>
-                <input placeholder="search..." className='test-input' type="search" value={inputVal} onChange={(e => setSearchVal(e.target.value))} />
+            <form className='test-form' action="" onSubmit={searchSubmit}>
+                <input placeholder="search..." className='test-input' type="search" value={inputVal} onChange={(e => setInputVal(e.target.value))} />
                 <i className="fa fa-search"></i>
             </form>
         </div>
