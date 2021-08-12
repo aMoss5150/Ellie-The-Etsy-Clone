ec49f384a832b22ba2f3f081d62658c8be2a6f5e
Andrew Moss <andrewmossaudio@gmail.com>
2021-08-12 17:42:13 -0500

    cart totals are now calculated dynamically

--- frontend/src/components/CartDisplay/index.js
+++ frontend/src/components/CartDisplay/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getCartLS } from '../../store/cart'
+import { getProducts } from '../../store/products'
 
 import CartSummary from '../CartSummary'
 import CartItem from '../CartItem'
@@ -10,9 +11,25 @@ import './CartDisplay.css'
 export default function CartDisplay() {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart)
+    const products = Object.values(useSelector(state => state.products))
+    // const product = products.filter((product) => product.id === +itemId)
+
+    let allCartItems = []
+    console.log(cart)
+    for (let i = 0; i <= cart.length; i++) {
+        let itemNum = cart[i]
+        let selected = products.find(prod => prod.id === itemNum)
+        selected && allCartItems.push(selected)
+
+        console.log(allCartItems)
+
+    }
+
 
     useEffect(() => {
         dispatch(getCartLS())
+        dispatch(getProducts())
+        return () => allCartItems = []
     }, [dispatch])
 
     if (!cart) {
@@ -23,7 +40,23 @@ export default function CartDisplay() {
     const cartArray = cart
     return (
         <div className="cart__page">
-            <div className="cart__breakdown">breakdown</div>
+            <div className="cart__breakdown">breakdown
+                parts total: {
+                    allCartItems.reduce((acc, el) => {
+                        return el.price + acc
+                    }, 0)
+                }
+                labor total estimate: {
+                    allCartItems.reduce((acc, el) => {
+                        return el.labor_estimate + acc
+                    }, 0)
+                }
+                build estimate: {
+                    allCartItems.reduce((acc, el) => {
+                        return el.price + el.labor_estimate + acc
+                    }, 0)
+                }
+            </div>
             <div className="cart__container">
                 {cartArray.map((item) => (
                     <CartItem key={item} itemId={item} />
