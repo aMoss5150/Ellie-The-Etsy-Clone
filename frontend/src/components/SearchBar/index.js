bea7dba4e81a933d0827bf1f7a876ff7282c145a
Andrew Moss <andrewmossaudio@gmail.com>
2021-08-16 15:30:39 -0500

    changed profile logo

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 import './SearchBar.css'
 
 export default function SearchBar() {
+    const history = useHistory()
     const [searchVal, setSearchVal] = useState('')
     const [inputVal, setInputVal] = useState('')
 
@@ -19,12 +21,15 @@ export default function SearchBar() {
 
     if (!products) return null
     else return (
-        <div className='search__bar-parent'>
-            <form className='test-form' action="" onSubmit={handleSearch}>
-                <input placeholder="search..." className='test-input' type="search" value={inputVal} onChange={(e => setSearchVal(e.target.value))} />
-                <i className="fa fa-search"></i>
-            </form>
-        </div>
+        <>
+            <span>{searchVal}</span>
+            <div className='search__bar-parent'>
+                <form className='test-form' action="" onSubmit={handleSearch}>
+                    <input placeholder="search..." className='test-input' type="search" value={inputVal} onChange={(e => setInputVal(e.target.value))} />
+                    <i className="fa fa-search"></i>
+                </form>
+            </div>
+        </>
     )
 
 }
