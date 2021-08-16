422b49b064955003c24b5d0859802ce2a8c6176a
Andrew Moss <andrewmossaudio@gmail.com>
2021-08-16 16:48:02 -0500

    re styling small things

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,34 +1,55 @@
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 
 import './SearchBar.css'
 
 export default function SearchBar() {
-    const history = useHistory()
-    const [searchVal, setSearchVal] = useState('')
     const [inputVal, setInputVal] = useState('')
+    const [matches, setMatches] = useState([])
+    const [prodsArr, setProdsArr] = useState([])
+
 
     const products = useSelector(state => state.products)
-
     const handleSearch = () => {
+        let possibles = []
+        Object.values(products).forEach((product) => {
+            let lengthOfSrch = inputVal.length
+            let possMatch = product.product_name.split('').slice(0, lengthOfSrch).join('')
 
+            if (possMatch.toLowerCase() === inputVal.toLowerCase() && inputVal.length > 0) {
+                possibles.push(product)
+            }
+        })
+
+        setMatches(possibles)
     }
+    // const searchSubmit = (e) => {
+    //     e.preventDefault()
+    // }
 
     useEffect(() => {
-        setSearchVal(inputVal)
+        handleSearch()
     }, [inputVal])
 
+    // setProdsArr(Array.from(products))
     if (!products) return null
     else return (
         <>
-            <span>{searchVal}</span>
             <div className='search__bar-parent'>
                 <form className='test-form' action="" onSubmit={handleSearch}>
                     <input placeholder="search..." className='test-input' type="search" value={inputVal} onChange={(e => setInputVal(e.target.value))} />
                     <i className="fa fa-search"></i>
                 </form>
             </div>
+            {matches &&
+                <>
+                    {/* <button onClick={() => setMatches('')}>Clear</button> */}
+                    <ul className="search__matches">{matches.map((m) => (
+                        <li onClick={() => setInputVal('')}><Link to={`/all-products/${m.id}`}>{m.product_name}</Link></li>
+                    ))}</ul>
+                </>
+            }
         </>
     )
 
