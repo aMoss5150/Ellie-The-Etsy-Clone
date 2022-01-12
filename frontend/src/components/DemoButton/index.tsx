e844f9a5706df536ea7e7a56cc20721dc39d6993
Andrew Moss <andrewmossaudio@gmail.com>
2022-01-12 16:16:17 -0600

    demo button refactored

--- frontend/src/components/DemoButton/index.tsx
+++ frontend/src/components/DemoButton/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import { useAppDispatch } from '../../hooks'
 import { login } from '../../store/session';
 
 
 export default function DemoButton() {
-    const dispatch = useDispatch()
+    const dispatch = useAppDispatch()
     const credential = "The Stig"
     const password = "passy12"
 
